82baaf8718dc6240c42f777ca4eca68910a6195c
22H51A05P6 <22h51a05p6@cmrcet.ac.in>
2024-12-08 13:52:33 +0530

    Update sai1.java

--- sai1.java
+++ sai1.java
@@ -1,6 +1,9 @@
 public class sai1 {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World11!");
+        System.out.println("Hello, World1!");
+        
+        
     }
 }
